36a6411c1645f11715546cf5ed5dec0ef4d91c90
Jwata <watanabe0621@gmail.com>
2017-09-28 00:54:47 +0900

    intergrate with APIs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
+import { listImages, addImage, deleteImage } from './awsApi';
 import logo from './logo.svg';
-import ImageList from './ImageList'
-import ImageForm from './ImageForm'
+import Image from './Image';
+import ImageForm from './ImageForm';
 import './App.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      images: [{
-        id: Math.random().toString(36).substring(7),
-        url: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
-      }]
-    };
+    this.state = { images: [] };
   }
 
-  addImage = (url) => {
-    const image = { id: Math.random().toString(36).substring(7), url: url }
-    this.setState(prevState => ({
-      images: prevState.images.concat(image),
-    }));
+  async componentDidMount() {
+    listImages().then((images) => {
+      this.setState({ images: images });
+    });
   }
 
-  removeImage = (id) => {
-    this.setState(prevState => ({
-      images: prevState.images.filter(image => image.id != id)
-    }));
+  renderImageList() {
+    return [{}].concat(this.state.images).map(
+        (image, i) =>
+          (i !== 0) ? <Image id={image.id} src={image.url} onDelete={this.deleteImage.bind(this)} /> : ''
+      )
+  }
+
+  async addImage(url) {
+    addImage(url).then((image) => {
+      this.setState(prevState => ({
+        images: prevState.images.concat(image),
+      }));
+    });
+  }
+
+  async deleteImage(id) {
+    deleteImage(id).then(() => {
+      this.setState(prevState => ({
+        images: prevState.images.filter(image => image.id != id)
+      }));
+    });
   }
 
   render() {
@@ -39,7 +51,9 @@ class App extends Component {
           <ImageForm onSubmit={this.addImage.bind(this)} url={this.state.url} />
         </div>
         <div className="section">
-          <ImageList images={this.state.images} onDelete={this.removeImage.bind(this)} />
+          <div className="columns">
+            {this.renderImageList()}
+          </div>
         </div>
       </div>
     );
---
+++ src/awsApi.js
@@ -0,0 +1,26 @@
+const endpoint = 'http://localhost:8080';
+
+export async function listImages() {
+  const path = `${endpoint}/images`;
+  return fetch(path, { method: 'GET' }).then((response) => {
+    return response.json().then((jsonResponse) => {
+      return jsonResponse.images;
+    });
+  });
+};
+
+export async function addImage(url) {
+  const path = `${endpoint}/images`;
+  const body = { url: url }
+  return fetch(path, { method: 'POST', body: JSON.stringify(body) }).then((response) => {
+    return response.json().then((jsonResponse) => {
+      return jsonResponse;
+    });
+  });
+};
+
+export async function deleteImage(id) {
+  const path = `${endpoint}/images`;
+  const body = { id: id }
+  return fetch(path, { method: 'DELETE', body: JSON.stringify(body) });
+};
